991a7d870c86aedaad93963cbbac731c75a0e438
Frank <frank.rodriguez60@gmail.com>
2021-03-22 16:04:14 -0600

    ok

--- Curso_Python_101/numeros.py
+++ Curso_Python_101/numeros.py
@@ -1,15 +1,19 @@
-numero = [1,2,3,4,5,6,7]
+numero = [1, 2, 3, 4, 5, 6, 7]
 
-def square(num):
-    resultado= num**2
-    return resultado
+# Global
+name = 'VARIABLE GLOBAL'
 
-def check_even(num):
-  return num%2 == 0
 
-for item in map(square, numero):
-  print(item)
-print(list(map(square,numero)))
+def saludo():
+    # Enclosing
+    name = 'Eric Alexander'
 
-for n in filter (check_even,numero):
-  print(n)
+    def hola():
+        # Local
+        name = 'Variable Local'
+        print('Hola ' + name)
+
+    hola
+
+
+saludo
