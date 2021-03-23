1bcc8460ea9a01cc4e4b574e723bd8134cead4c3
Frank <frank.rodriguez60@gmail.com>
2021-03-22 18:46:02 -0600

    listo el problema de funciones

--- Curso_Python_101/nuevo.py
+++ Curso_Python_101/nuevo.py
@@ -1,2 +1,25 @@
-saludo = '¡Hola Mundo!'
-print (saludo)
+
+saludo ='Hola '
+name = 'frank '
+last = 'Rosas'
+
+def mi_funcion():
+    saludo = 'Que tal '
+    name = 'Maitane '
+    last = 'Rodriguez '
+    print('n1 '+saludo + name + last)
+    
+    def mi_segunda_funcion():
+        global last
+        saludo = 'Como estas '
+        name = 'Angie '
+        last = 'del Bosque '
+class        print ('n2 '+saludo+name)
+    mi_segunda_funcion()
+print(saludo+name+ last)
+mi_funcion()
+print(saludo+name+ last)
+mi_funcion()
+
+
+
--- Curso_Python_101/numeros.py
+++ Curso_Python_101/numeros.py
@@ -1,19 +1,14 @@
-numero = [1, 2, 3, 4, 5, 6, 7]
 
 # Global
 name = 'VARIABLE GLOBAL'
-
-
 def saludo():
     # Enclosing
     name = 'Eric Alexander'
-
+    print (name)
     def hola():
         # Local
         name = 'Variable Local'
         print('Hola ' + name)
 
-    hola
-
-
-saludo
+    hola ()
+saludo ()
